eed88381fc27cef9bbe8b3a88a57d692e7f04bfe
kucinginsom <kucinginsom@gmail.com>
2022-06-29 15:47:54 +0700

    - Cleaning code. add fitur restart and validation di Class Hopimpa - add abstract class di class parent untuk tes fitur abstraction fitur dan method

--- src/test/kotlin/Suit.kt
+++ src/test/kotlin/Suit.kt
@@ -1,14 +1,17 @@
 abstract class Suit(val game: String ) {
-
     init{
         println("Mulai Bermain: $game")
     }
-
     fun resultGameWin(player: Int, description: String){
         var playerWin = player+1
         playerWin = playerWin ?: 1
         println("Hasil: ")
         println("Pemain: $playerWin $description")
     }
+    fun getDrawResult(){
+        println("Hasil: ")
+        println("DRAW")
+    }
+     abstract fun restartGame()
 
 }
